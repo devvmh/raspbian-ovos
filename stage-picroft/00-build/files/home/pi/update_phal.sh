fffe7dea51e3675d539d68eb7eef614efed9725f
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-03-10 18:06:54 +0000

    pi-gen automation - downloadable images (#4)
    * image build workflow
    
    * upload test
    
    * upload test
    
    * fix host
    
    * fix path
    
    * rm scripts, now in ovos-utils entrypoints
    
    * user ovos
    
    * user ovos
    
    * upload path
    
    * id: build

---
+++ stage-picroft/00-build/files/home/pi/update_phal.sh
@@ -0,0 +1,11 @@
+pip3 install -U --pre ovos_PHAL
+pip3 install -U --pre ovos-phal-plugin-system
+pip3 install -U --pre ovos-phal-plugin-connectivity-events
+pip3 install -U --pre ovos-PHAL-plugin-ipgeo
+pip3 install -U --pre ovos-PHAL-plugin-oauth
+pip3 install -U --pre ovos-phal-plugin-dashboard
+pip3 install -U --pre ovos-phal-plugin-notification-widgets
+pip3 install -U --pre ovos-phal-plugin-brightness-control-rpi
+pip3 install -U --pre ovos-phal-plugin-alsa
+pip3 install -U --pre ovos-phal-plugin-gpsd
+# pip3 install -U --pre ovos-phal-plugin-analog-media-devices

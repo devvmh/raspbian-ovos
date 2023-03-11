1a732bcb36309b9c7fcbb0a2c73a27167942f0ae
builderjer <builderjer@gmail.com>
2023-03-11 07:03:38 -0700

    removed old files

--- stage-picroft/00-build/files/home/pi/update_ovos.sh
+++
@@ -1,9 +0,0 @@
-pip3 install -U --pre --no-deps ovos-core[audio,PHAL,stt,tts,skills_lgpl,skills,bus,skills-essential]
-pip3 install -U --pre ovos-config
-pip3 install -U --pre ovos-utils
-pip3 install -U --pre ovos-backend-client
-pip3 install -U --pre ovos-bus-client
-pip3 install -U --pre ovos-plugin-manager
-pip3 install -U --pre ovos-workshop
-pip3 install -U --pre ovos-lingua-franca
-

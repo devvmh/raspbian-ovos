ba67fcbab138f7be2f432746f6aa22c5e83ff0d8
builderjer <builderjer@gmail.com>
2023-06-23 12:42:30 -0600

    missed one

--- stage-audio/02-voice/01-run.sh
+++ stage-audio/02-voice/01-run.sh
@@ -11,4 +11,4 @@ install -v -m 0644 hey_mycroft_v0.1.onnx "${ROOTFS_DIR}/home/ovos/.local/share/o
 install -v -m 0644 files/ovos-dinkum-listener.service "${ROOTFS_DIR}/etc/systemd/user/ovos-dinkum-listener.service"
 install -v -m 0755 files/ovos-systemd-dinkum-listener "${ROOTFS_DIR}/usr/libexec/ovos-systemd-dinkum-listener"
 
-echo "enable ovos-dinkum-listener.service" >> "${ROOTFS_DIR}/etc/systemd/user/user-preset/10-ovos-user.preset"
+echo "enable ovos-dinkum-listener.service" >> "${ROOTFS_DIR}/etc/systemd/user-preset/10-ovos-user.preset"

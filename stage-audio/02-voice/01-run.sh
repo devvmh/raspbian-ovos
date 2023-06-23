b19ce0afdd2ec3a1b8c3a797ed4738c9a74acb10
builderjer <builderjer@gmail.com>
2023-06-23 08:17:44 -0600

    remove predownloaded hey_mycroft.onnx

--- stage-audio/02-voice/01-run.sh
+++ stage-audio/02-voice/01-run.sh
@@ -3,7 +3,10 @@
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share"
 
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/openwakeword"
-install -v -m 0644 files/hey_mycroft_v0.1.onnx "${ROOTFS_DIR}/home/ovos/.local/share/openwakeword/"
+
+wget https://github.com/dscripka/openWakeWord/blob/main/openwakeword/resources/models/hey_mycroft_v0.1.onnx
+
+install -v -m 0644 hey_mycroft_v0.1.onnx "${ROOTFS_DIR}/home/ovos/.local/share/openwakeword/"
 
 install -v -m 0644 files/ovos-dinkum-listener.service "${ROOTFS_DIR}/etc/systemd/user/ovos-dinkum-listener.service"
 install -v -m 0755 files/ovos-systemd-dinkum-listener "${ROOTFS_DIR}/usr/libexec/ovos-systemd-dinkum-listener"

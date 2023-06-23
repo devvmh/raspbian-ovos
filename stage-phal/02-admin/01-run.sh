62d520a7c3c882e817de50dac459c24a3f38903b
builderjer <34875857+builderjer@users.noreply.github.com>
2023-06-23 09:02:12 -0600

    Update 01-run.sh
    typo

--- stage-phal/02-admin/01-run.sh
+++ stage-phal/02-admin/01-run.sh
@@ -3,5 +3,5 @@
 install -v -m 0755 files/ovos-systemd-admin-phal "${ROOTFS_DIR}/usr/libexec/ovos-systemd-admin-phal"
 install -v -m 0644 files/ovos-admin-phal.service "${ROOTFS_DIR}/etc/systemd/system/ovos-admin-phal.service"
 
-echo "enable ovos-admin-phal.service" >> "${ROOTFS_DIR}/etc/systemd/system/system/10-ovos-system.preset"
+echo "enable ovos-admin-phal.service" >> "${ROOTFS_DIR}/etc/systemd/system/10-ovos-system.preset"
 

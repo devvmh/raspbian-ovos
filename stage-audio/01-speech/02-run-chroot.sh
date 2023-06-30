7ef5d69ddb536be56732d55259777e33b01717fa
builderjer <34875857+builderjer@users.noreply.github.com>
2023-06-30 06:38:14 -0600

    Update 02-run-chroot.sh
    added tts-plugin-server

--- stage-audio/01-speech/02-run-chroot.sh
+++ stage-audio/01-speech/02-run-chroot.sh
@@ -1,5 +1,3 @@
 pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-piper
-pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic
-
-
+pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-server-plugin

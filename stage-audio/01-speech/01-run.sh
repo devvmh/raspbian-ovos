5ff693b6722708f3381d000b1d85d49abb292f74
builderjer <builderjer@gmail.com>
2023-07-01 10:46:35 -0600

    config mods

--- stage-audio/01-speech/01-run.sh
+++ stage-audio/01-speech/01-run.sh
@@ -12,11 +12,11 @@ install -v -d -m 0755 "${ROOTFS_DIR}/etc/udev/rules.d"
 install -v -m 0644 files/91-pulseaudio-GeneralPlus.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
 
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share"
-install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/piper"
-install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/piper/voices"
+install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts"
+install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts/voice-en-gb-alan-low"
 
 wget https://github.com/rhasspy/piper/releases/download/v0.0.2/voice-en-gb-alan-low.tar.gz
-tar -xf voice-en-gb-alan-low.tar.gz -C "${ROOTFS_DIR}/home/ovos/.local/share/piper/voices"
+tar -xf voice-en-gb-alan-low.tar.gz -C "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts/voice-en-gb-alan-low"
 rm voice-en-gb-alan-low.tar.gz
 
 #install -v -m 0644 files/en-gb-alan-low.onnx "${ROOTFS_DIR}/home/ovos/.local/share/piper/voices/en-gb-alan-low.onnx"

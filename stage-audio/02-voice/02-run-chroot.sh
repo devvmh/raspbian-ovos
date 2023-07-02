235b974468477fc1ce9659326e2c9cfc10daa225
builderjer <34875857+builderjer@users.noreply.github.com>
2023-07-02 12:22:48 -0600

    Update 02-run-chroot.sh
    added precise-lite ww

--- stage-audio/02-voice/02-run-chroot.sh
+++ stage-audio/02-voice/02-run-chroot.sh
@@ -1,10 +1,6 @@
 pip3 install git+https://github.com/OpenVoiceOS/ovos-dinkum-listener
-
 pip3 install git+https://github.com/OpenVoiceOS/ovos-vad-plugin-silero
-
 pip3 install git+https://github.com/OpenVoiceOS/ovos-stt-plugin-server
-
 pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-openWakeWord
-
+pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite
 pip3 install git+https://github.com/OpenVoiceOS/ovos-microphone-plugin-alsa
-

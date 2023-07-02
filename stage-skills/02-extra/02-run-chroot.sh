56a31663b33b22bf50a63f071d338c32c140cad9
builderjer <builderjer@gmail.com>
2023-07-02 13:42:35 -0600

    no setup.py on dictation-skill

--- stage-skills/02-extra/02-run-chroot.sh
+++ stage-skills/02-extra/02-run-chroot.sh
@@ -1,6 +1,6 @@
 # ovos skills
 pip3 install git+https://github.com/OpenVoiceOS/ovos-skill-easter-eggs
-pip3 install git+https://github.com/OpenVoiceOS/skill-ovos-dictation
+pip3 install git+https://github.com/OpenVoiceOS/skill-ovos-dictation@tests
 
 # jarbas skills
 pip3 install git+https://github.com/JarbasSkills/skill-icanhazdadjokes

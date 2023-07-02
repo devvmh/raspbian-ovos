de639db93fffd809ddf6fe761b7375d84a338edf
builderjer <builderjer@gmail.com>
2023-07-02 12:00:54 -0600

    additions
    Added more skills
    added timestamp to image name
    fixed admin-phal from not starting

---
+++ stage-skills/02-extra/02-run-chroot.sh
@@ -0,0 +1,15 @@
+# ovos skills
+pip3 install git+https://github.com/OpenVoiceOS/ovos-skill-easter-eggs
+pip3 install git+https://github.com/OpenVoiceOS/skill-ovos-dictation
+
+# jarbas skills
+pip3 install git+https://github.com/JarbasSkills/skill-icanhazdadjokes
+
+# neon skills
+pip3 install git+https://github.com/NeonGeckoCom/skill-stock
+pip3 install git+https://github.com/NeonGeckoCom/skill-user_settings
+pip3 install git+https://github.com/NeonGeckoCom/skill-speed_test
+pip3 install git+https://github.com/NeonGeckoCom/skill-spelling
+pip3 install git+https://github.com/NeonGeckoCom/skill-caffeinewiz
+pip3 install git+https://github.com/NeonGeckoCom/skill-translation
+pip3 install git+https://github.com/NeonGeckoCom/skill-audio_record

1ee594cf81cc77dcc15bf4eabcf4dd1eb3a863a1
builderjer <builderjer@gmail.com>
2023-07-02 12:33:05 -0600

    tflite instead of tensorflow

--- stage-audio/01-speech/02-run-chroot.sh
+++ stage-audio/01-speech/02-run-chroot.sh
@@ -2,5 +2,3 @@ pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
 pip3 install git+https://github.com/builderjer/ovos-tts-plugin-piper
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-server-plugin
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic
-
-pip3 install tensorflow
--- stage-audio/02-voice/02-run-chroot.sh
+++ stage-audio/02-voice/02-run-chroot.sh
@@ -4,3 +4,6 @@ pip3 install git+https://github.com/OpenVoiceOS/ovos-stt-plugin-server
 pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-openWakeWord
 pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite
 pip3 install git+https://github.com/OpenVoiceOS/ovos-microphone-plugin-alsa
+
+# custom tflite_runtime to work with python 3.9 and raspbian bullseye
+pip3 install -f 'https://whl.smartgic.io/' tflite_runtime

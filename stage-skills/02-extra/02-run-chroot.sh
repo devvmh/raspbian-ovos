3656f1228a274a899dd2f1ec3182e0f293796cfe
builderjer <34875857+builderjer@users.noreply.github.com>
2023-07-03 14:09:53 -0600

    Update 02-run-chroot.sh
    remove neon skills
    
    heavy for this image

--- stage-skills/02-extra/02-run-chroot.sh
+++ stage-skills/02-extra/02-run-chroot.sh
@@ -6,10 +6,13 @@ pip3 install git+https://github.com/OpenVoiceOS/skill-ovos-dictation@tests
 pip3 install git+https://github.com/JarbasSkills/skill-icanhazdadjokes
 
 # neon skills
-pip3 install git+https://github.com/NeonGeckoCom/skill-stock
-pip3 install git+https://github.com/NeonGeckoCom/skill-user_settings
-pip3 install git+https://github.com/NeonGeckoCom/skill-speed_test
-pip3 install git+https://github.com/NeonGeckoCom/skill-spelling
-pip3 install git+https://github.com/NeonGeckoCom/skill-caffeinewiz
-pip3 install git+https://github.com/NeonGeckoCom/skill-translation
-pip3 install git+https://github.com/NeonGeckoCom/skill-audio_record
+
+# These pull a lot of stuff that make this image heavy
+# Uncomment to install
+#pip3 install git+https://github.com/NeonGeckoCom/skill-stock
+#pip3 install git+https://github.com/NeonGeckoCom/skill-user_settings
+#pip3 install git+https://github.com/NeonGeckoCom/skill-speed_test
+#pip3 install git+https://github.com/NeonGeckoCom/skill-spelling
+#pip3 install git+https://github.com/NeonGeckoCom/skill-caffeinewiz
+#pip3 install git+https://github.com/NeonGeckoCom/skill-translation
+#pip3 install git+https://github.com/NeonGeckoCom/skill-audio_record

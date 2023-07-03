f38f56c96e0a772e5467f7b04c0c2961ee7c4cac
builderjer <34875857+builderjer@users.noreply.github.com>
2023-07-03 14:12:17 -0600

    Update 02-run-chroot.sh
    too many dependencies for mark 1 with neon skills

--- stage-skills/02-extra/02-run-chroot.sh
+++ stage-skills/02-extra/02-run-chroot.sh
@@ -6,10 +6,12 @@ pip3 install git+https://github.com/OpenVoiceOS/skill-ovos-dictation@tests
 pip3 install git+https://github.com/JarbasSkills/skill-icanhazdadjokes
 
 # neon skills
-pip3 install git+https://github.com/NeonGeckoCom/skill-stock
-pip3 install git+https://github.com/NeonGeckoCom/skill-user_settings
-pip3 install git+https://github.com/NeonGeckoCom/skill-speed_test
-pip3 install git+https://github.com/NeonGeckoCom/skill-spelling
-pip3 install git+https://github.com/NeonGeckoCom/skill-caffeinewiz
-pip3 install git+https://github.com/NeonGeckoCom/skill-translation
-pip3 install git+https://github.com/NeonGeckoCom/skill-audio_record
+
+# Removing due to heavy dependencies on a mark 1
+#pip3 install git+https://github.com/NeonGeckoCom/skill-stock
+#pip3 install git+https://github.com/NeonGeckoCom/skill-user_settings
+#pip3 install git+https://github.com/NeonGeckoCom/skill-speed_test
+#pip3 install git+https://github.com/NeonGeckoCom/skill-spelling
+#pip3 install git+https://github.com/NeonGeckoCom/skill-caffeinewiz
+#pip3 install git+https://github.com/NeonGeckoCom/skill-translation
+#pip3 install git+https://github.com/NeonGeckoCom/skill-audio_record

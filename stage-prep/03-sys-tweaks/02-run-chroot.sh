085057a248f5af94df4e836131cc3f1411dd2851
builderjer <builderjer@gmail.com>
2023-07-03 14:04:19 -0600

    remove mk1 stuff

--- stage-prep/03-sys-tweaks/02-run-chroot.sh
+++ stage-prep/03-sys-tweaks/02-run-chroot.sh
@@ -5,14 +5,11 @@ cd ..
 rm -rf zram-swap
 
 cat >> /boot/config.txt << EOF
+
 gpu_mem=16
 disable_splash=1
 
 [pi3]
-
-# Disable Bluetooth, it interferes with serial port
-dtoverlay=pi3-disable-bt
-dtoverlay=pi3-miniuart-bt
 
 force_turbo=1
 boot_delay=1

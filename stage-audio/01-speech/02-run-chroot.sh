30c61571f0102e2bf3aa8b1b521ac30dcfbfda46
builderjer <builderjer@gmail.com>
2023-07-04 10:57:57 -0600

    minor changes for mk-1

--- stage-audio/01-speech/02-run-chroot.sh
+++ stage-audio/01-speech/02-run-chroot.sh
@@ -1,5 +1,5 @@
 pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
-pip3 install git+https://github.com/builderjer/ovos-tts-plugin-piper
+pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-piper
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-server-plugin
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic
 pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-pocketsphinx

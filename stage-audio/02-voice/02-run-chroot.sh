c88317806964578cc5b0cfb8e80ee7f2b614f3dc
builderjer <builderjer@gmail.com>
2023-07-04 08:26:13 -0600

    whoops

--- stage-audio/02-voice/02-run-chroot.sh
+++ stage-audio/02-voice/02-run-chroot.sh
@@ -1,9 +1,10 @@
+# custom tflite_runtime to work with python 3.9 and raspbian bullseye
+pip3 install -U -f 'https://whl.smartgic.io/' tflite_runtime
+
 pip3 install git+https://github.com/OpenVoiceOS/ovos-dinkum-listener
 pip3 install git+https://github.com/OpenVoiceOS/ovos-vad-plugin-silero
 pip3 install git+https://github.com/OpenVoiceOS/ovos-stt-plugin-server
 pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-openWakeWord
 pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite
+pip3 install git+https://github.com/OpenVoiceOS/ovos-ww-plugin-pocketsphinx
 pip3 install git+https://github.com/OpenVoiceOS/ovos-microphone-plugin-alsa
-
-# custom tflite_runtime to work with python 3.9 and raspbian bullseye
-pip3 install -U -f 'https://whl.smartgic.io/' tflite_runtime

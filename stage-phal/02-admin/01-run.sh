c6bbefb986c787f1801a6c0d728bfff3ed9cbdb8
builderjer <builderjer@gmail.com>
2023-07-19 06:54:41 -0600

    fix admin phal

--- stage-phal/02-admin/01-run.sh
+++ stage-phal/02-admin/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 install -v -m 0755 files/ovos-systemd-admin-phal "${ROOTFS_DIR}/usr/libexec/ovos-systemd-admin-phal"
-install -v -m 0644 files/ovos-admin-phal.service "${ROOTFS_DIR}/etc/systemd/system/ovos-admin-phal.service"
+install -v -m 0644 files/ovos-admin-phal.service "${ROOTFS_DIR}/etc/systemd/user/ovos-admin-phal.service"
 
-echo "enable ovos-admin-phal.service" >> "${ROOTFS_DIR}/etc/systemd/system-preset/10-ovos-system.preset"
+echo "enable ovos-admin-phal.service" >> "${ROOTFS_DIR}/etc/systemd/user-preset/10-ovos-user.preset"
 

efb605ece7d3f5ec3bbca219764b128591d0bc26
builderjer <34875857+builderjer@users.noreply.github.com>
2023-07-23 21:06:55 -0600

    Update 02-run-chroot.sh
    add phal-ipgeo

--- stage-phal/01-user/02-run-chroot.sh
+++ stage-phal/01-user/02-run-chroot.sh
@@ -3,3 +3,5 @@ pip3 install git+https://github.com/OpenVoiceOS/ovos-phal
 pip3 install git+https://github.com/OpenVoiceOS/ovos-PHAL-plugin-alsa
 pip3 install git+https://github.com/OpenVoiceOS/ovos-PHAL-plugin-balena-wifi
 pip3 install git+https://github.com/OpenVoiceOS/ovos-PHAL-plugin-wifi-setup
+
+pip3 install git+https://github.com/OpenVoiceOS/ovos-PHAL-plugin-ipgeo

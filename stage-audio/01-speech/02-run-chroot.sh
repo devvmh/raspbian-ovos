8eed84daecfcbc99645a98ce182cd1662d5a6757
builderjer <34875857+builderjer@users.noreply.github.com>
2023-08-24 10:47:37 -0600

    added padatious (#79)

--- stage-audio/01-speech/02-run-chroot.sh
+++ stage-audio/01-speech/02-run-chroot.sh
@@ -2,3 +2,6 @@ pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
 pip3 install git+https://github.com/builderjer/ovos-tts-plugin-piper
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-server-plugin
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic
+
+pip3 install padatious
+pip3 install fann2==1.0.7

19f5a688461f57821623d05b3f576653ac605a43
builderjer <builderjer@gmail.com>
2023-08-29 06:52:28 -0600

    added vlc and plugin for better audio playback

--- stage-audio/01-speech/02-run-chroot.sh
+++ stage-audio/01-speech/02-run-chroot.sh
@@ -2,6 +2,7 @@ pip3 install git+https://github.com/OpenVoiceOS/ovos-audio
 pip3 install git+https://github.com/builderjer/ovos-tts-plugin-piper
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-server-plugin
 pip3 install git+https://github.com/OpenVoiceOS/ovos-tts-plugin-mimic
+pip3 install git+https://github.com/OpenVoiceOS/ovos-vlc-plugin
 
 pip3 install espeak_phonemizer
 

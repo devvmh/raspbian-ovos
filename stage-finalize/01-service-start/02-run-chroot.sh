3ea68cbcafb93ba64cc854282ed2289d43c1e5dc
builderjer <34875857+builderjer@users.noreply.github.com>
2023-09-02 11:00:52 -0600

    fix syslinks to logs (#84)

--- stage-finalize/01-service-start/02-run-chroot.sh
+++ stage-finalize/01-service-start/02-run-chroot.sh
@@ -8,5 +8,8 @@ su -c "systemctl --user preset-all" --login ovos
 
 su -c "ln -s /ramdisk/mycroft/audio.log /home/ovos/.local/state/mycroft/" --login ovos
 su -c "ln -s /ramdisk/mycroft/bus.log /home/ovos/.local/state/mycroft/" --login ovos
+su -c "ln -s /ramdisk/mycroft/ovos.log /home/ovos/.local/state/mycroft/" --login ovos
+su -c "ln -s /ramdisk/mycroft/phal_admin.log /home/ovos/.local/state/mycroft/" --login ovos
+su -c "ln -s /ramdisk/mycroft/phal.log /home/ovos/.local/state/mycroft/" --login ovos
 su -c "ln -s /ramdisk/mycroft/skills.log /home/ovos/.local/state/mycroft/" --login ovos
 su -c "ln -s /ramdisk/mycroft/voice.log /home/ovos/.local/state/mycroft/" --login ovos
